a3ac6f0e1676a1141af7a1961628b8b08a66ca65
Didter <13160668588@163.com>
2021-06-18 23:25:46 +0800

    添加权限控制页面

---
+++ src/views/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="index">
+         <router-link to="/">v-has 权限控制</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+a {
+    color: rgb(0, 185, 218);
+}
+</style>
